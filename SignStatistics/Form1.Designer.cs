f7a23ecb2f5bdb6d0aee71b3f17540f6b96e42ac
sunpwork <sunpwork@qq.com>
2019-04-02 14:02:53 +0800

    init program

---
+++ SignStatistics/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace SignStatistics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.signTableButton = new System.Windows.Forms.Button();
+            this.allMemberTableButton = new System.Windows.Forms.Button();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.signTableFileNameTxb = new System.Windows.Forms.TextBox();
+            this.allMemberTableFileNameTxb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // signTableButton
+            // 
+            this.signTableButton.Location = new System.Drawing.Point(348, 36);
+            this.signTableButton.Name = "signTableButton";
+            this.signTableButton.Size = new System.Drawing.Size(75, 23);
+            this.signTableButton.TabIndex = 0;
+            this.signTableButton.Text = "签到表";
+            this.signTableButton.UseVisualStyleBackColor = true;
+            this.signTableButton.Click += new System.EventHandler(this.signTableButton_Click);
+            // 
+            // allMemberTableButton
+            // 
+            this.allMemberTableButton.Location = new System.Drawing.Point(348, 68);
+            this.allMemberTableButton.Name = "allMemberTableButton";
+            this.allMemberTableButton.Size = new System.Drawing.Size(75, 23);
+            this.allMemberTableButton.TabIndex = 1;
+            this.allMemberTableButton.Text = "全部成员";
+            this.allMemberTableButton.UseVisualStyleBackColor = true;
+            this.allMemberTableButton.Click += new System.EventHandler(this.allMemberTableButton_Click);
+            // 
+            // exportButton
+            // 
+            this.exportButton.Location = new System.Drawing.Point(81, 135);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(75, 23);
+            this.exportButton.TabIndex = 2;
+            this.exportButton.Text = "开始";
+            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // signTableFileNameTxb
+            // 
+            this.signTableFileNameTxb.Enabled = false;
+            this.signTableFileNameTxb.Location = new System.Drawing.Point(81, 38);
+            this.signTableFileNameTxb.Name = "signTableFileNameTxb";
+            this.signTableFileNameTxb.Size = new System.Drawing.Size(251, 21);
+            this.signTableFileNameTxb.TabIndex = 3;
+            // 
+            // allMemberTableFileNameTxb
+            // 
+            this.allMemberTableFileNameTxb.Enabled = false;
+            this.allMemberTableFileNameTxb.Location = new System.Drawing.Point(81, 68);
+            this.allMemberTableFileNameTxb.Name = "allMemberTableFileNameTxb";
+            this.allMemberTableFileNameTxb.Size = new System.Drawing.Size(251, 21);
+            this.allMemberTableFileNameTxb.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "签到表";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "全部成员";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(81, 103);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(342, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "进度";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 170);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.allMemberTableFileNameTxb);
+            this.Controls.Add(this.signTableFileNameTxb);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.allMemberTableButton);
+            this.Controls.Add(this.signTableButton);
+            this.Name = "Form1";
+            this.Text = "签到表生成";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button signTableButton;
+        private System.Windows.Forms.Button allMemberTableButton;
+        private System.Windows.Forms.Button exportButton;
+        private System.Windows.Forms.TextBox signTableFileNameTxb;
+        private System.Windows.Forms.TextBox allMemberTableFileNameTxb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SignStatistics/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace SignStatistics
+{
+    public partial class Form1 : Form
+    {
+        private String signTableFileName;
+        private String allMemberTableFileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void signTableButton_Click(object sender, EventArgs e)
+        {
+            this.signTableFileName = this.showExcelFileDialog();
+            this.signTableFileNameTxb.Text = this.signTableFileName;
+        }
+
+        private void allMemberTableButton_Click(object sender, EventArgs e)
+        {
+            this.allMemberTableFileName = this.showExcelFileDialog();
+            this.allMemberTableFileNameTxb.Text = this.allMemberTableFileName;
+        }
+
+        private String showExcelFileDialog()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "请选择文件";
+            openFileDialog.Filter = "所有文件(*xls*)|*.xls*"; //设置要选择的文件的类型
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                return openFileDialog.FileName;
+            }
+            return null;
+        }
+
+        // 修改进度条值
+        private delegate void setProgressBarValue_dg(int value);
+        private void setProgressBarValue(int value)
+        {
+            this.progressBar1.Value = value;
+        }
+
+        private delegate void setExportButtonEnable_dg(bool value);
+        private void setExportButtonEnable(bool enable)
+        {
+            this.exportButton.Enabled = enable;
+        }
+
+        private void exportButton_Click(object sender, EventArgs e)
+        {
+            if (this.signTableFileName == null)
+            {
+                MessageBox.Show("请先上传签到表！");
+                return;
+            }
+            else if (this.allMemberTableFileName == null)
+            {
+                MessageBox.Show("请先上传全部成员表！");
+                return;
+            }
+
+            if (this.signTableFileName == this.allMemberTableFileName)
+            {
+                MessageBox.Show("两份表格相同，请确认后重试！");
+                return;
+            }
+            Task task = Task.Run(() =>
+            {
+                // 禁用按钮
+                this.Invoke(new setExportButtonEnable_dg(setExportButtonEnable), false);
+                // 还原进度条
+                this.Invoke(new setProgressBarValue_dg(setProgressBarValue), 0);
+
+                Excel.Application excel = new Excel.Application();
+                Excel.Workbook signTableBook = excel.Workbooks.Open(signTableFileName);
+                Excel.Workbook allMemberTableBook = excel.Workbooks.Open(allMemberTableFileName);
+                Excel.Worksheet signTableSheet = (Excel.Worksheet)signTableBook.Worksheets[1];
+                Excel.Worksheet allMemberTableSheet = (Excel.Worksheet)allMemberTableBook.Worksheets[1];
+
+                signTableSheet.Cells[1, 2].Value2 = "单位";
+                int signTableIndex = 2;
+
+                //Dictionary<string, List<String>> signUserDic = new Dictionary<string, List<String>>();
+                //while (signTableSheet.Cells[signTableIndex, 1].Value2 != null)
+                //{
+                //    String name = signTableSheet.Cells[signTableIndex, 1].Value2.ToString();
+                //    List<String> signUserDepartmentList = new List<string>();
+                //    int allMemberTableIndex = 2;
+                //    int setIndex = 2;// 部门的字段位置
+                //    while (allMemberTableSheet.Cells[allMemberTableIndex, 1].Value2 != null)
+                //    {
+                //        if (allMemberTableSheet.Cells[allMemberTableIndex, 1].Value2.ToString() == name)
+                //        {
+                //            //signTableSheet.Cells[signTableIndex, setIndex].Value2 = allMemberTableSheet.Cells[allMemberTableIndex, 2].Value2;
+                //            signUserDepartmentList.Add(allMemberTableSheet.Cells[allMemberTableIndex, 2].Value2);
+                //            setIndex++;
+                //        }
+                //        allMemberTableIndex++;
+                //    }
+                //    signUserDic.Add(name, signUserDepartmentList);
+                //    //设置进度条的值
+                //    this.Invoke(new setProgressBarValue_dg(setProgressBarValue), (int)(signTableIndex * 100 / signTableSheet.UsedRange.Rows.Count));
+                //    signTableIndex++;
+                //}
+                //var sortedDict = signUserDic.OrderBy(x => x.Value[0]).ToDictionary(x => x.Key, x => x.Value);
+
+                Dictionary<string, List<String>> departmentDic = new Dictionary<string, List<String>>();
+                List<String> repetitionNameList = new List<string>();
+                departmentDic.Add("无对应部门", new List<string>());
+                while (signTableSheet.Cells[signTableIndex, 1].Value2 != null)
+                {
+                    String name = signTableSheet.Cells[signTableIndex, 1].Value2.ToString();
+                    int allMemberTableIndex = 2;
+                    List<String> departmentList = new List<string>();
+                    while (allMemberTableSheet.Cells[allMemberTableIndex, 1].Value2 != null)
+                    {
+                        if (allMemberTableSheet.Cells[allMemberTableIndex, 1].Value2.ToString() == name)
+                        {
+                            String department = allMemberTableSheet.Cells[allMemberTableIndex, 2].Value2;
+                            if (departmentDic.ContainsKey(department) && !departmentDic[department].Contains(name))
+                            {
+                                departmentDic[department].Add(name);
+                            }
+                            else
+                            {
+                                List<String> nameList = new List<String>();
+                                nameList.Add(name);
+                                departmentDic.Add(department, nameList);
+                            }
+                            departmentList.Add(department);
+                        }
+                        allMemberTableIndex++;
+                    }
+                    if (departmentList.Count == 0)
+                    {
+                        departmentDic["无对应部门"].Add(name);
+                    }
+                    else if (departmentList.Count > 1)
+                    {
+                        repetitionNameList.Add(name);
+                    }
+
+                    //设置进度条的值
+                    this.Invoke(new setProgressBarValue_dg(setProgressBarValue), (int)(signTableIndex * 100 / signTableSheet.UsedRange.Rows.Count));
+                    signTableIndex++;
+                }
+                signTableBook.Close(true);
+                allMemberTableBook.Close(true);
+
+                Excel.Workbook exportWorkbook = excel.Workbooks.Add(Type.Missing);
+                Excel.Worksheet exportWorksheet = exportWorkbook.Worksheets.Add(Type.Missing);
+                exportWorksheet.Cells[1, 1].Value2 = "部门";
+                exportWorksheet.Cells[1, 2].Value2 = "姓名";
+                exportWorksheet.Cells[1, 3].Value2 = "人数";
+
+                int departmentCellIndex = 2;
+                foreach (var item in departmentDic)
+                {
+                    if (item.Value.Count > 0)
+                    {
+                        // 合并部门单元格
+                        Excel.Range departmentRange = exportWorksheet.Cells.Range[exportWorksheet.Cells[departmentCellIndex, 1], exportWorksheet.Cells[departmentCellIndex + item.Value.Count - 1, 1]];
+                        departmentRange.Value2 = Type.Missing;
+                        departmentRange.Merge(Type.Missing);
+                        departmentRange.Value2 = item.Key;
+
+                        // 合并人数单元格
+                        Excel.Range countRange = exportWorksheet.Cells.Range[exportWorksheet.Cells[departmentCellIndex, 3], exportWorksheet.Cells[departmentCellIndex + item.Value.Count - 1, 3]];
+                        countRange.Value2 = Type.Missing;
+                        countRange.Merge(Type.Missing);
+                        countRange.Value2 = item.Value.Count;
+
+                        // 填入姓名
+                        foreach (String name in item.Value)
+                        {
+                            exportWorksheet.Cells.Cells[departmentCellIndex, 2].Value2 = name;
+                            if (repetitionNameList.Contains(name))
+                            {
+                                exportWorksheet.Range[exportWorksheet.Cells[departmentCellIndex, 2], exportWorksheet.Cells[departmentCellIndex, 3]].Interior.ColorIndex = 3;
+                            }
+                            departmentCellIndex++;
+                        }
+                    }
+                }
+
+                exportWorkbook.Close(true);
+
+                excel.Quit();
+                // 安全回收进程
+                System.GC.GetGeneration(excel);
+
+                // 导出完成
+                this.Invoke(new setProgressBarValue_dg(setProgressBarValue), 100);
+                this.Invoke(new setExportButtonEnable_dg(setExportButtonEnable), true);
+                MessageBox.Show("导出成功！");
+            });
+        }
+    }
+}
